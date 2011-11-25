460153a42568b663f3557bf1ddb20f73739c2e07
Simon Walker <s.r.walker101@googlemail.com>
2011-11-25 15:57:01 +0000

    started new file

---
+++ OffsetDistribution.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+'''
+Calculate the distribution of fractions in the centre pixel for a psf of
+given size as the psf is moved away from the centre pixel
+'''
+
+
+def Gaussian2D(y, x, fwhm, offset):
+    sigma = fwhm / 2.35
+    arg1 = (x-offset[0])**2
+    arg2 = (y - offset[1])**2
+    return exp(-(arg1 + arg2) / (2. * sigma**2))
+
+
+
+class App(object):
+    """docstring for App"""
+    def __init__(self, args):
+        super(App, self).__init__()
+        self.args = args
+
+    def __del__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--device", help="PGPLOT device",
+            required=False, type=str, default="1/xs")
+    args = parser.parse_args()
+    app = App(args)
+
