ae7203f9a12ff409105025cf9c2e29482ea4f3f3
Simon Walker <s.r.walker101@googlemail.com>
2011-11-25 16:36:13 +0000

    plotting the distribution

--- OffsetDistribution.py
+++ OffsetDistribution.py
@@ -24,14 +24,15 @@ class App(object):
         super(App, self).__init__()
         self.args = args
         self.fwhm = 1.5
-        self.N = 100
+        self.N = self.args.niter
 
-        xRange = [-3.*self.fwhm, 3.*self.fwhm]
-        yRange = [-3.*self.fwhm, 3.*self.fwhm]
+        xRange = [-2.*self.fwhm, 2.*self.fwhm]
+        yRange = [-2.*self.fwhm, 2.*self.fwhm]
 
         self.xvals = (xRange[1] - xRange[0]) * np.random.random(self.N) + xRange[0]
         self.yvals = (yRange[1] - yRange[0]) * np.random.random(self.N) + yRange[0]
 
+        pgopen(self.args.device)
         self.run()
 
     def Integrate(self, lims, offset):
@@ -39,7 +40,7 @@ class App(object):
 
 
     def __del__(self):
-        pass
+        pgclos()
 
     def run(self):
         total = self.Integrate((-np.Inf, np.Inf, -np.Inf, np.Inf), (0., 0.))[0]
@@ -47,7 +48,24 @@ class App(object):
         fractions = []
         for y in self.yvals:
             for x in self.xvals:
-                fractions.append(Integrate((-0.5, 0.5, -0.5, 0.5), (x, y)) / total)
+                Fraction = self.Integrate((-0.5, 0.5, -0.5, 0.5), (x, y))[0] / total
+                print "%.1f %.1f => %.3f" % (x, y, Fraction)
+                fractions.append(Fraction)
+
+        fractions = np.log10(fractions)
+
+
+        vals, edges = np.histogram(fractions, bins=50, range=(-4, 0))
+        centres = edges[:-1] + np.diff(edges)[0] / 2.
+        pgenv(-4, 0, 0, 1.1*vals.max(), 0, 10)
+
+        pgsci(15)
+        pgerrb(6, centres, vals, np.sqrt(vals), 1.0)
+        pgsci(1)
+        pgbin(centres, vals, True)
+
+
+
 
 
 
@@ -55,6 +73,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--device", help="PGPLOT device",
             required=False, type=str, default="1/xs")
+    parser.add_argument("-N", "--niter", help="Number of iterations",
+            required=False, type=int, default=100)
     args = parser.parse_args()
     app = App(args)
 
