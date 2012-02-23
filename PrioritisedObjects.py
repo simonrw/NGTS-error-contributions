937e21b3d3a28cc0039a20b552be95b79c97f6dd
Simon Walker <s.r.walker101@googlemail.com>
2012-02-23 15:27:24 +0000

    adding script
    prioritises the objects based on magnitude

---
+++ PrioritisedObjects.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+Assign a weight to each object
+for a specific exposure time and count up 
+the total sum for each exposure time.
+
+The hightest weight wins
+'''
+
+import NHighPrecisionObjects
+import numpy as np
+import matplotlib.pyplot as plt
+
+def weight(mag, t, bonus, importance=[0.05,  0.3, 1.]):
+    '''
+    Weight is inversely proportional to the magnitude
+    ie brighter objects are more important
+
+    Add a value if the target is in the high precision 
+    range, and subtract a value if the target is saturated
+    '''
+    rangeVals = NHighPrecisionObjects.rangeAtExptime(t)
+    saturated = mag < rangeVals[0]
+    highprecision = (mag <= rangeVals[1]) & (np.logical_not(saturated))
+
+    weight = np.poly1d(importance)(mag.max() - mag)
+    weight[saturated] -= bonus
+    weight[highprecision] += bonus
+
+    return weight
+
+
+parser = NHighPrecisionObjects.NOMADDataStore()
+
+bonus = 15
+importance = [1., 0.]
+
+linestyles = ['-', '--', ':']
+
+for i, field in enumerate([1, 2, 3]):
+    parser.setField(field)
+    objects = parser.visible()
+    exptime, weights = zip(*[
+        (e, np.sum(weight(objects, e, bonus=bonus, importance=importance))) 
+        for e in np.linspace(5, 90, 150)])
+
+
+
+
+    #exptime = np.array(exptime)
+    #weights = np.array(weights)
+
+
+    plt.plot(exptime, np.log10(weights), color='k', ls=linestyles[i],
+            label="NOMAD %d" % field)
+    plt.xlabel("Exposure time / s")
+    plt.ylabel("log10 Weight")
+
+parser.close()
+#plt.title("Max at %.2fs" % exptime[weights==weights.max()])
+plt.legend(loc='best')
+plt.show()
+
