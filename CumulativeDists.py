55643d8ebeaef3ad7e89d7c38e8ae9e03a3c0f83
Simon Walker <s.r.walker101@googlemail.com>
2012-06-08 14:53:54 +0100

    now plotting each line
    was a typo meaning that only the last line was getting plotted

--- CumulativeDists.py
+++ CumulativeDists.py
@@ -30,20 +30,20 @@ def main(args):
         'precisiondata.cpickle')) as filedata:
         exptimes, crosspoints, satpoints = cPickle.load(filedata)
 
-    x_range = [8, 16]
+    x_range = [9, 14]
 
     interpcross = interp1d(exptimes, crosspoints, kind='linear')
     interpsat = interp1d(exptimes, satpoints, kind='linear')
 
     selection_cut = '((typ == 4) | (typ == 5) | (typ == 6)) & (cl == 5)'
 
-    linestyles = [2, 3, 4]
-    exptimes = [10, 20, 30]
+    N = 5
+    colours = np.arange(2, 2 + N, 1)
+    exptimes = np.arange(1, N + 1) * 10
 
     with open_bparser() as bp, pgh.open_plot('1/xs'):
 
         pg.pgvstd()
-        pg.pgswin(x_range[0], x_range[1], 0, 1.1)
 
         nodes = [bp.getTable('/fields', 'field{:d}'.format(i))
                 for i in xrange(1, 4)]
@@ -55,18 +55,23 @@ def main(args):
 
         all_vmags = np.array(all_vmags)
 
-        for exptime, ls in zip(exptimes, linestyles):
+        pg.pgswin(x_range[0], x_range[1], 0, 0.04)
+        for exptime, colour in zip(exptimes, colours):
             satpoint = interpsat(exptime)
             crosspoint = interpcross(exptime)
 
-            selected = all_vmags[(all_vmags > satpoint) & (all_vmags <
+            selected = all_vmags[(all_vmags > satpoint) & (all_vmags <=
                 crosspoint)]
 
-            xdata, ydata = cumulative_hist(np.array(selected),
-                    min_val=x_range[0], max_val=x_range[1])
 
-        with pgh.change_linestyle(ls):
-            pg.pgbin(xdata, ydata, False)
+            xdata, ydata = cumulative_hist(np.array(selected),
+                    min_val=x_range[0], max_val=x_range[1], norm=len(all_vmags))
+
+            #ydata, xdata = np.histogram(selected, 20)
+            #xdata = xdata[:-1]
+
+            with pgh.change_colour(colour):
+                pg.pgbin(xdata, ydata, False)
 
 
         pg.pgbox('bcnst', 0, 0, 'bcnst', 0, 0)
@@ -76,10 +81,10 @@ def main(args):
         pg.pgsvp(0.7, 0.9, 0.1, 0.3)
         pg.pgswin(0., 1., 0., 1.)
 
-        for i, (exptime, ls) in enumerate(zip(exptimes, linestyles)):
+        for i, (exptime, colour) in enumerate(zip(exptimes, colours)):
             yval = 0.1 + 0.8 * i / len(exptimes)
 
-            with pgh.change_linestyle(ls):
+            with pgh.change_colour(colour):
                 pg.pgline(np.array([0.2, 0.4]), np.ones(2) * yval)
 
             pg.pgtext(0.5, yval, r'{:d} s'.format(exptime))
