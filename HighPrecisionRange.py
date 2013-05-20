c31db226214bbb92981ed85a580bcb055263a87a
Simon Walker <s.r.walker101@googlemail.com>
2013-05-20 16:46:04 +0100

    Rendering bright points also

--- HighPrecisionRange.py
+++ HighPrecisionRange.py
@@ -10,7 +10,6 @@ Besancon model and real field data
 
 import subprocess as sp
 import os.path
-import matplotlib.pyplot as plt
 import numpy as np
 import multiprocessing
 import argparse
@@ -32,9 +31,12 @@ def getStatsForExptime(time):
     p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
     linestr, err = p.communicate()
     lines = linestr.split("\n")
+
     crosspoint = float(filter(lambda line: "CROSSPOINT" in line, lines)[0].split()[-1])
-    satpoint = float(filter(lambda line: "DARK" in line, lines)[0].split()[-1])
-    return crosspoint, satpoint
+    darkpoint = float(filter(lambda line: "DARK" in line, lines)[0].split()[-1])
+    brightpoint = float(filter(lambda line: 'BRIGHT' in line, lines)[0].split()[-1])
+
+    return crosspoint, darkpoint, brightpoint
 
 class App(object):
     """Main application"""
@@ -62,8 +64,6 @@ class App(object):
 
 
         self._args = args
-        self.fig = plt.figure(figsize=(11, 8))
-        self.ax = self.fig.add_subplot(111)
 
         self.pool = multiprocessing.Pool()
 
@@ -71,11 +71,12 @@ class App(object):
 
 
     def run(self):
-        crosspoints, satpoints = zip(*self.pool.map(getStatsForExptime, self._exptimes))
+        crosspoints, darkpoints, brightpoints = zip(*map(getStatsForExptime, self._exptimes))
 
         self._exptimes = np.array(self._exptimes)
         self.crosspoints = np.array(crosspoints)
-        self.satpoints = np.array(satpoints)
+        self.darkpoints = np.array(darkpoints)
+        self.brightpoints = np.array(brightpoints)
 
         self.dumpData()
 
@@ -86,7 +87,8 @@ class App(object):
         '''
         Dump the data to a cpickle file
         '''
-        cPickle.dump(np.array([self._exptimes, self.crosspoints, self.satpoints]),
+        cPickle.dump(np.array([self._exptimes, self.crosspoints, self.darkpoints,
+            self.brightpoints]),
                 open("precisiondata.cpickle", "w"),
                 protocol=2)
 
@@ -99,8 +101,6 @@ class App(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", help="Save the figure",
-            type=str, default=None, required=False)
     args = parser.parse_args()
     app = App(args)
     app.run()
