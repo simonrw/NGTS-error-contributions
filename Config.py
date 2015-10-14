6389ba089341de345c2397640c684c53cc7251b1
Simon Walker <s.r.walker101@googlemail.com>
2015-10-14 16:17:05 +0100

    Update config for final instrument

--- Config.py
+++ Config.py
@@ -12,16 +12,16 @@ logging.basicConfig(level=logging.INFO)
 
 _detector = NGTSDetector()
 
-Gain = 2.1
+Gain = 2.9
 HorizontalSpeed = _detector.horizspeed
 VerticalTime = _detector.verttime
 NAXIS1, NAXIS2 = _detector.ccdsize
 ReadTime = _detector.readTime()
 ReadNoise = 15. # e- per pix
-FWHM = 1.0
+FWHM = 1.6
 Radius = 1.5 * FWHM # pixels
 Area = pi * Radius**2
-BiasLevelADU = 1667 # ADU
+BiasLevelADU = 1300 # ADU
 BiasLevel = BiasLevelADU * Gain
 Digitisation = 16
 ElectronicSatur = ((2**Digitisation - 1)) * Area
